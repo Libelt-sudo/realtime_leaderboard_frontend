fe1bcafad110f6aeb7b814b178d92e68c46430e8
Libelt-sudo <seibet.07@gmail.com>
2026-07-04 00:08:26 +0100

    removed the '|| localhost:3000' from the BASE_URL asignment.

--- src/api/leaderboard.ts
+++ src/api/leaderboard.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 async function handleResponse(res: Response) {
   const json = await res.json();
